80758dd8e0f45461b885f9d9573a2659cce62608
Niels Steenbergen <n.steenbergen@uu.nl>
2020-12-15 14:38:37 +0100

    Single source of truth on RDF namespaces.
    Edit only the mapping to gain module attributes, graph prefix binding
    and namespace lookup.

--- workflow_synthesis/rdf_namespaces.py
+++ workflow_synthesis/rdf_namespaces.py
@@ -3,28 +3,34 @@ This module holds the RDF namespaces that we use frequently.
 """
 
 import rdflib
+import sys
 
-_gk = "http://geographicknowledge.de/vocab/"
+NAMESPACES = {
+    k: rdflib.Namespace(v) for k, v in {
+        "test": "http://www.semanticweb.org/test#",
+        "foaf": "http://xmlns.com/foaf/0.1/",
+        "ccd": "http://geographicknowledge.de/vocab/CoreConceptData.rdf#",
+        "owl": "http://www.w3.org/2002/07/owl#",
+        "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+        "xml": "http://www.w3.org/XML/1998/namespace",
+        "xsd": "http://www.w3.org/2001/XMLSchema#",
+        "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+        "exm": "http://geographicknowledge.de/vocab/ExtensiveMeasures.rdf#",
+        "em": "http://geographicknowledge.de/vocab/ExtensiveMeasures.rdf#",
+        "ada": "http://geographicknowledge.de/vocab/AnalysisData.rdf#",
+        "wf": "http://geographicknowledge.de/vocab/Workflow.rdf#",
+        "gis": "http://geographicknowledge.de/vocab/GISConcepts.rdf#",
+        "tools": "http://geographicknowledge.de/vocab/GISTools.rdf#",
+    }.items()
+}
 
-TEST = rdflib.Namespace("http://www.semanticweb.org/test#")
-CCD = rdflib.Namespace(_gk+"CoreConceptData.rdf#")
-TOOLS = rdflib.Namespace(_gk+"GISTools.rdf#")
-ADA = rdflib.Namespace(_gk+"AnalysisData.rdf#")
-EXM = rdflib.Namespace(_gk+"ExtensiveMeasures.rdf#")
-WF = rdflib.Namespace(_gk+"Workflow.rdf#")
+# Programmatically set namespaces as constants of this module
+for k, v in NAMESPACES.items():
+    setattr(sys.modules[__name__], k.upper(), v)
 
 
 def setprefixes(g):
-    g.bind('foaf', 'http://xmlns.com/foaf/0.1/')
-    g.bind('ccd', 'http://geographicknowledge.de/vocab/CoreConceptData.rdf#')
-    g.bind('owl', 'http://www.w3.org/2002/07/owl#')
-    g.bind('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#')
-    g.bind('xml', 'http://www.w3.org/XML/1998/namespace')
-    g.bind('xsd', 'http://www.w3.org/2001/XMLSchema#')
-    g.bind('rdfs', 'http://www.w3.org/2000/01/rdf-schema#')
-    g.bind('exm', 'http://geographicknowledge.de/vocab/ExtensiveMeasures.rdf#')
-    g.bind('ada', 'http://geographicknowledge.de/vocab/AnalysisData.rdf#')
-    g.bind('wf', 'http://geographicknowledge.de/vocab/Workflow.rdf#')
-    g.bind('gis', 'http://geographicknowledge.de/vocab/GISConcepts.rdf#')
-    g.bind('tools', 'http://geographicknowledge.de/vocab/GISTools.rdf#')
+    for k, v in NAMESPACES.items():
+        g.bind(k, str(v))
     return g
+
