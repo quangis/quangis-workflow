c916394dc195842b0fc6dcc1e54f1fec61fd328b
Niels Steenbergen <n.steenbergen@uu.nl>
2021-01-06 17:49:24 +0100

    More encapsulation.

--- workflow_synthesis/ffi.py
+++ workflow_synthesis/ffi.py
@@ -1,6 +1,6 @@
 """
 This is a wrapper to APE, the Java application that is used for workflow
-synthesis. 
+synthesis. It interfaces with a JVM.
 """
 # This approach was chosen because we need to:
 # -  run multiple solutions one after another
@@ -17,11 +17,11 @@ synthesis.
 from rdf_namespaces import CCD, WF, TOOLS, setprefixes
 
 import rdflib
-from rdflib import BNode, URIRef
-from rdflib.namespace import RDF
+from rdflib import BNode, URIRef, Graph
+from rdflib.namespace import RDF, Namespace
 import jpype
 import jpype.imports
-from jpype.types import JString
+from typing import Iterable, Tuple, Mapping
 
 # We need version 1.1.2's API; lower versions won't work
 jpype.startJVM(classpath=['lib/APE-1.1.2-executable.jar'])
@@ -31,58 +31,106 @@ from java.util import Arrays
 from org.json import JSONObject, JSONArray
 import nl.uu.cs.ape.sat
 import nl.uu.cs.ape.sat.configuration
-from nl.uu.cs.ape.sat.models import Type
+import nl.uu.cs.ape.sat.models
+import nl.uu.cs.ape.sat.utils
+from nl.uu.cs.ape.sat.core.solutionStructure import SolutionWorkflow
+
+
+class Datatype:
+    """
+    A semantic datatype, in multiple dimensions.
+    """
+
+    def __init__(self, mapping: Mapping[URIRef, Iterable[URIRef]]):
+        """
+        We represent a datatype as a mapping from RDF dimension nodes to one or
+        more of its subclasses.
+        """
+        self._mapping = mapping
+
+    def to_java(self,
+                setup: nl.uu.cs.ape.sat.utils.APEDomainSetup,
+                is_output: bool = False) -> nl.uu.cs.ape.sat.models.Type:
+
+        obj = JSONObject()
+        for dimension, subclasses in self._mapping.items():
+            a = JSONArray()
+            for c in subclasses:
+                a.put(str(c))
+            obj.put(str(dimension), a)
+
+        return nl.uu.cs.ape.sat.models.Type.taxonomyInstanceFromJson(
+            obj, setup, is_output)
+
+
+class Workflow:
+    """
+    A single workflow.
+    """
+
+    def __init__(self, wf: SolutionWorkflow):
+        """
+        Internally, we just store APE's SolutionWorkflow.
+        """
+        self._wf = wf
+
+    def to_rdf(self):
+        """
+        Represent as an RDF graph.
+        """
+        pass
 
 
 class APE:
+    """
+    Wrapper class for APE.
+    """
 
-    def __init__(self):
+    def __init__(self,
+                 taxonomy: str,
+                 tools: str,
+                 namespace: Namespace,
+                 tool_root: URIRef,
+                 dimensions: Iterable[URIRef]):
+
         self.config = nl.uu.cs.ape.sat.configuration.APECoreConfig(
-            File("build/GISTaxonomy.rdf"),
-            "http://geographicknowledge.de/vocab/CoreConceptData.rdf#",
-            "http://geographicknowledge.de/vocab/GISTools.rdf#Tool",
-            Arrays.asList("CoreConceptQ", "LayerA", "NominalA"),
-            File("build/ToolDescription.json"),
+            File(taxonomy),
+            str(namespace),
+            str(tool_root),
+            Arrays.asList(*map(str, dimensions)),
+            File(tools),
             True
         )
         self.ape = nl.uu.cs.ape.sat.APE(self.config)
         self.setup = self.ape.getDomainSetup()
 
-    def run(self, inputs, outputs):
+    def run(self,
+            inputs: Iterable[Datatype],
+            outputs: Iterable[Datatype],
+            solution_length: Tuple[int, int] = (1, 10),
+            max_solutions: int = 100) -> Iterable[Graph]:
 
-        inputs_ = Arrays.asList(*map(self.as_type, inputs))
-        outputs_ = Arrays.asList(*map(self.as_type, outputs))
+        inp = Arrays.asList(*(i.to_java(self.setup, False) for i in inputs))
+        out = Arrays.asList(*(o.to_java(self.setup, True) for o in outputs))
 
         config = nl.uu.cs.ape.sat.configuration.APERunConfig\
             .builder()\
-            .withSolutionDirPath("build/")\
+            .withSolutionDirPath(".")\
             .withConstraintsJSON(JSONObject())\
-            .withSolutionMinLength(1)\
-            .withSolutionMaxLength(10)\
-            .withMaxNoSolutions(100)\
-            .withProgramInputs(inputs_)\
-            .withProgramOutputs(outputs_)\
+            .withSolutionMinLength(solution_length[0])\
+            .withSolutionMaxLength(solution_length[1])\
+            .withMaxNoSolutions(max_solutions)\
+            .withProgramInputs(inp)\
+            .withProgramOutputs(out)\
             .withApeDomainSetup(self.setup)\
             .build()
 
-        jsolutions = self.ape.runSynthesis(config)
+        solutions = self.ape.runSynthesis(config)
 
         return [
-            workflow_as_rdf(jsolutions.get(i))
-            for i in range(0, jsolutions.getNumberOfSolutions())
+            workflow_as_rdf(solutions.get(i))
+            for i in range(0, solutions.getNumberOfSolutions())
         ]
-
-    def as_type(self, mapping):
-        """
-        Turn a dictionary into an input/output Type for APE.
-        """
-        obj = JSONObject()
-        for k, v in mapping.items():
-            a = JSONArray()
-            for s in v:
-                a.put(JString(s))
-            obj.put(JString(k), a)
-        return Type.taxonomyInstanceFromJson(obj, self.setup, False)
 
 
 def resource_node(t, g, resources):
@@ -163,22 +211,27 @@ def fix_toolid(toolid):
     return str(toolid).strip("\"").split("[tool]")[0]
 
 
-ape = APE()
+ape = APE(
+    taxonomy="build/GISTaxonomy.rdf",
+    tools="build/ToolDescription.json",
+    tool_root=TOOLS.Tool,
+    namespace=CCD,
+    dimensions=(CCD.CoreConceptQ, CCD.LayerA, CCD.NominalA)
+)
 ape.run(
     inputs=[
-        {
-            "CoreConceptQ": ["CoreConceptQ"],
-            "LayerA": ["LayerA"],
-            "NominalA": ["RatioA"]
-        },
+        Datatype({
+            CCD.CoreConceptQ: [CCD.CoreConceptQ],
+            CCD.LayerA: [CCD.LayerA],
+            CCD.NominalA: [CCD.RatioA]
+        }),
     ],
     outputs=[
-        {
-            "CoreConceptQ": ["CoreConceptQ"],
-            "LayerA": ["LayerA"],
-            "NominalA": ["PlainRatioA"]
-        }
+        Datatype({
+            CCD.CoreConceptQ: [CCD.CoreConceptQ],
+            CCD.LayerA: [CCD.LayerA],
+            CCD.NominalA: [CCD.PlainRatioA]
+        })
     ]
 )
-
 
