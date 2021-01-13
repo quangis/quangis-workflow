99ecdca55422f20f534f589dfaaf8fbc7f5e1d33
Niels Steenbergen <n.steenbergen@uu.nl>
2021-01-13 19:45:15 +0100

    Invert programmatic definition of namespaces.
    Instead of first providing the mapping and setting the attributes based
    on that, we now take the opposite approach and make the mapping based on
    the attributes.
    
    This makes mypy happy --- it now understands that there really is such a
    thing as `namespace.CCD`.

--- quangis/namespace.py
+++ quangis/namespace.py
@@ -3,29 +3,25 @@ This module holds the RDF namespaces that we use frequently.
 """
 
 import sys
-import rdflib
+from rdflib import Namespace
 
+TEST = Namespace("http://www.semanticweb.org/test#")
+FOAF = Namespace("http://xmlns.com/foaf/0.1/")
+CCD = Namespace("http://geographicknowledge.de/vocab/CoreConceptData.rdf#")
+OWL = Namespace("http://www.w3.org/2002/07/owl#")
+RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+XML = Namespace("http://www.w3.org/XML/1998/namespace")
+XSD = Namespace("http://www.w3.org/2001/XMLSchema#")
+RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+EXM = Namespace("http://geographicknowledge.de/vocab/ExtensiveMeasures.rdf#")
+EM = Namespace("http://geographicknowledge.de/vocab/ExtensiveMeasures.rdf#")
+ADA = Namespace("http://geographicknowledge.de/vocab/AnalysisData.rdf#")
+WF = Namespace("http://geographicknowledge.de/vocab/Workflow.rdf#")
+GIS = Namespace("http://geographicknowledge.de/vocab/GISConcepts.rdf#")
+TOOLS = Namespace("http://geographicknowledge.de/vocab/GISTools.rdf#")
 
+# Also provide a mapping for easy programmatic access
 mapping = {
-    k: rdflib.Namespace(v) for k, v in {
-        "test": "http://www.semanticweb.org/test#",
-        "foaf": "http://xmlns.com/foaf/0.1/",
-        "ccd": "http://geographicknowledge.de/vocab/CoreConceptData.rdf#",
-        "owl": "http://www.w3.org/2002/07/owl#",
-        "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-        "xml": "http://www.w3.org/XML/1998/namespace",
-        "xsd": "http://www.w3.org/2001/XMLSchema#",
-        "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
-        "exm": "http://geographicknowledge.de/vocab/ExtensiveMeasures.rdf#",
-        "em": "http://geographicknowledge.de/vocab/ExtensiveMeasures.rdf#",
-        "ada": "http://geographicknowledge.de/vocab/AnalysisData.rdf#",
-        "wf": "http://geographicknowledge.de/vocab/Workflow.rdf#",
-        "gis": "http://geographicknowledge.de/vocab/GISConcepts.rdf#",
-        "tools": "http://geographicknowledge.de/vocab/GISTools.rdf#",
-    }.items()
+    k.lower(): v for k, v in sys.modules[__name__].__dict__.items()
+    if isinstance(v, Namespace)
 }
-
-
-# Programmatically set namespaces as uppercased constants of this module
-for k, v in mapping.items():
-    setattr(sys.modules[__name__], k.upper(), v)
