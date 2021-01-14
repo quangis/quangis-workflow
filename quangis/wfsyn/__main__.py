20935c1a8ec24bcd7b87d484075e163f0901cf1b
Niels Steenbergen <n.steenbergen@uu.nl>
2021-01-14 11:59:43 +0100

    Remove indentation in __main__.py
    It is pointless at this point.

--- quangis/wfsyn/__main__.py
+++ quangis/wfsyn/__main__.py
@@ -11,6 +11,7 @@ import os.path
 import argparse
 import logging
 import urllib.request
+from importlib import reload
 
 from quangis.wfsyn import wfsyn
 from quangis.ontology import Ontology
@@ -37,94 +38,91 @@ def download_if_missing(path: str, url: str) -> str:
     return path
 
 
-if __name__ == '__main__':
+parser = argparse.ArgumentParser(
+    description="Wrapper for APE that synthesises CCD workflows"
+)
 
-    parser = argparse.ArgumentParser(
-        description="Wrapper for APE that synthesises CCD workflows"
+parser.add_argument(
+    '--types',
+    help="core concept datatype ontology in RDF")
+
+parser.add_argument(
+    '--tools',
+    help="tool annotation ontology in RDF")
+
+parser.add_argument(
+    '-d', '--dimension',
+    nargs='+',
+    type=uri,
+    default=[CCD.CoreConceptQ, CCD.LayerA, CCD.NominalA],
+    help="semantic dimensions")
+
+parser.add_argument(
+    '-n', '--solutions',
+    type=int,
+    default=5,
+    help="number of solution workflows attempted of find")
+
+parser.add_argument(
+    '--log',
+    choices=['debug', 'info', 'warning', 'error', 'critical'],
+    default='info',
+    help="Level of information logged to the terminal")
+
+args = parser.parse_args()
+
+# To force logging on this package; TODO prettier solution
+reload(logging)
+logging.basicConfig(
+    level=getattr(logging, args.log.upper()),
+    format="\033[1m%(asctime)s \033[0m%(message)s",
+)
+
+if not args.types:
+    args.types = download_if_missing(
+       path="CoreConceptData.rdf",
+       url="http://geographicknowledge.de/vocab/CoreConceptData.rdf"
     )
 
-    parser.add_argument(
-        '--types',
-        help="core concept datatype ontology in RDF")
-
-    parser.add_argument(
-        '--tools',
-        help="tool annotation ontology in RDF")
-
-    parser.add_argument(
-        '-d', '--dimension',
-        nargs='+',
-        type=uri,
-        default=[CCD.CoreConceptQ, CCD.LayerA, CCD.NominalA],
-        help="semantic dimensions")
-
-    parser.add_argument(
-        '-n', '--solutions',
-        type=int,
-        default=5,
-        help="number of solution workflows attempted of find")
-
-    parser.add_argument(
-        '--log',
-        choices=['debug', 'info', 'warning', 'error', 'critical'],
-        default='info',
-        help="Level of information logged to the terminal")
-
-    args = parser.parse_args()
-
-    # To force logging on this package; TODO prettier solution
-    from importlib import reload
-    reload(logging)
-    logging.basicConfig(
-        level=getattr(logging, args.log.upper()),
-        format="\033[1m%(asctime)s \033[0m%(message)s",
+if not args.tools:
+    args.tools = download_if_missing(
+        path="ToolDescription.ttl",
+        url="https://raw.githubusercontent.com/simonscheider/"
+            "QuAnGIS/master/ToolRepository/ToolDescription.ttl"
     )
 
-    if not args.types:
-        args.types = download_if_missing(
-           path="CoreConceptData.rdf",
-           url="http://geographicknowledge.de/vocab/CoreConceptData.rdf"
-        )
+logging.info("Dimensions: {}".format(", ".join(
+    map(shorten, args.dimension)
+)))
 
-    if not args.tools:
-        args.tools = download_if_missing(
-            path="ToolDescription.ttl",
-            url="https://raw.githubusercontent.com/simonscheider/"
-                "QuAnGIS/master/ToolRepository/ToolDescription.ttl"
-        )
-
-    logging.info("Dimensions: {}".format(", ".join(
-        map(shorten, args.dimension)
-    )))
-
-    data = [
-        (
-            [
-                SemType({
-                    CCD.CoreConceptQ: [CCD.CoreConceptQ],
-                    CCD.LayerA: [CCD.LayerA],
-                    CCD.NominalA: [CCD.RatioA]
-                })
-            ],
-            [
-                SemType({
-                    CCD.CoreConceptQ: [CCD.CoreConceptQ],
-                    CCD.LayerA: [CCD.LayerA],
-                    CCD.NominalA: [CCD.PlainRatioA]
-                })
-            ]
-        )
-    ]
-
-    solutions = wfsyn(
-        io=data,
-        solutions=args.solutions,
-        types=Ontology.from_rdf(args.types),
-        tools=Ontology.from_rdf(args.tools),
-        dimensions=args.dimension,
+data = [
+    (
+        [
+            SemType({
+                CCD.CoreConceptQ: [CCD.CoreConceptQ],
+                CCD.LayerA: [CCD.LayerA],
+                CCD.NominalA: [CCD.RatioA]
+            })
+        ],
+        [
+            SemType({
+                CCD.CoreConceptQ: [CCD.CoreConceptQ],
+                CCD.LayerA: [CCD.LayerA],
+                CCD.NominalA: [CCD.PlainRatioA]
+            })
+        ]
     )
+]
 
-    for g in solutions:
-        for s in g:
-            print("Solution:")
-            print(s.to_rdf().serialize(format="turtle").decode("utf-8"))
+solutions = wfsyn(
+    io=data,
+    solutions=args.solutions,
+    types=Ontology.from_rdf(args.types),
+    tools=Ontology.from_rdf(args.tools),
+    dimensions=args.dimension,
+)
+
+for g in solutions:
+    for s in g:
+        print("Solution:")
+        print(s.to_rdf().serialize(format="turtle").decode("utf-8"))
