9d686f7fe2c97c766a75cb82e5e18b0008d2ee4f
Niels Steenbergen <n.steenbergen@uu.nl>
2021-01-25 19:23:12 +0100

    Add signatures for other functions.
    This shows that it will need some work for what types the functions will
    actually get. It seems like subtypes should really be part of the typing
    system.

--- quangis/cct/algebra.py
+++ quangis/cct/algebra.py
@@ -25,18 +25,18 @@ class Algebra(object):
 x, y, z = (TypeVar.new() for _ in range(0, 3))
 
 # Entity value types
-E = partial(TypeOperator)
-V = E("entity")
-O = E("object")  # type: ignore
-S = E("region")
-L = E("location")
-Q = E("quality")
-Nom = E("nominal")
-Bool = E("boolean")
-Ord = E("ordinal")
-Count = E("count")
-Ratio = E("ratio")
-Itv = E("interval")
+Entity = partial(TypeOperator)
+V = Entity("entity")
+O = Entity("object")  # type: ignore
+S = Entity("region")
+L = Entity("location")
+Q = Entity("quality")
+Nom = Entity("nominal")
+Bool = Entity("boolean")
+Ord = Entity("ordinal")
+Count = Entity("count")
+Ratio = Entity("ratio")
+Itv = Entity("interval")
 
 # Relation types and type synonyms
 R = partial(TypeOperator, "rel")
@@ -73,23 +73,58 @@ constructors: Dict[str, AlgebraType] = {
 # Functions are type transformations in the algebra, without any particular
 # implementation
 functions = {
-    "compose":
-        (y ** z) ** (x ** y) ** (x ** z),
-    "ratio":
-        Ratio ** Ratio ** Ratio,
-    "avg":
-        R(x, Itv) ** Itv | {x: Subtype(V)},
-    "count":
-        R(O) ** Ratio,
-    "sigma_eq":
-        x ** y ** x | {x: Subtype(Q), y: Contains(x)},
-    "groupby_L":
-        (R(x) ** Q) ** R(x, Q, y) ** R(y) | {x: Subtype(V), y: Subtype(V)},
-    "join":
-        x ** R(y) ** x | {y: Subtype(V), x: Contains(y)},
-    "invert":
-        R(L, Ord) ** R(Ord, S)  # R(L, Nom) ** R(S, Nom)
+    # functional
+    "compose": (y ** z) ** (x ** y) ** (x ** z),
+
+    # derivations
+    "ratio": Ratio ** Ratio ** Ratio,
+
+    # aggregations of collections
+    "count": R(O) ** Ratio,
+    "size": R(L) ** Ratio,
+    "merge": R(S) ** S,
+    "centroid": R(L) ** L,
+
+    # statistical operations
+    "avg": R(x, Itv) ** Itv | {x: Subtype(V)},
+    "min": R(x, Ord) ** Ord | {x: Subtype(V)},
+    "max": R(x, Ord) ** Ord | {x: Subtype(V)},
+
+    # conversions
+    "reify": R(L) ** S,
+    "deify": S ** R(L),
+    "get": R(x) ** x | {x: Subtype(V)},
+    "invert": R(L, Ord) ** R(Ord, S),  # TODO overload R(L, Nom) ** R(S, Nom)
+    "revert": R(Ord, S) ** R(L, Ord),  # TODO overload
+
+    # quantified relations
+    "oDist": R(O, S) ** R(O, S) ** R(O, Ratio, O),
+    "lDist": R(L) ** R(L) ** R(L, Ratio, L),
+    "loDist": R(L) ** R(O, S) ** R(L, Ratio, O),
+    "oTopo": R(O, S) ** R(O, S) ** R(O, Nom, O),
+    "loTopo": R(L) ** R(O, S) ** R(L, Nom, O),
+    "nDist": R(O) ** R(O) ** R(O, Ratio, O) ** R(O, Ratio, O),
+    "lVis": R(L) ** R(L) ** R(L, Itv) ** R(L, Bool, L),
+    "interpol": R(S, Itv) ** R(L) ** R(L, Itv),
+
+    # amount operations
+    "fcont": R(L, Itv) ** Ratio,
+    "ocont": R(O, Ratio) ** Ratio,
+
+    # relational
+    "pi1": x ** y | {y: Contains(x, at=1)},
+    "pi2": x ** y | {y: Contains(x, at=2)},
+    "pi3": x ** y | {y: Contains(x, at=3)},
+    "sigmae": x ** y ** x | {x: Subtype(Q), y: Contains(x)},
+    "sigmale": x ** y ** x | {x: Subtype(Ord), y: Contains(x)},
+    "bowtie": x ** R(y) ** x | {y: Subtype(V), x: Contains(y)},
+    "bowtie*":
+        R(x, y, x) ** R(x, y) ** R(x, y, x) | {y: Subtype(Q), x: Subtype(V)},
+    "bowtie_": (Q ** Q ** Q) ** R(V, Q) ** R(V, Q) ** R(V, Q),
+    "groupbyL":
+        (R(y, Q) ** Q) ** R(x, Q, y) ** R(x, Q) | {x: Subtype(V), y: Subtype(V)},
+    "groupbyR":
+        (R(x, Q) ** Q) ** R(x, Q, y) ** R(y, Q) | {x: Subtype(V), y: Subtype(V)},
 }
 
 algebra = Algebra(constructors, functions)
-print(algebra.parse("ratio (ratioV a) (ratioV b)"))
