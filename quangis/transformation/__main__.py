5e2761a3689d5559f4c5e424715f68d7470eca7e
Niels Steenbergen <n.steenbergen@uu.nl>
2021-02-02 14:15:27 +0100

    Change __main__ for testing.

--- quangis/transformation/__main__.py
+++ quangis/transformation/__main__.py
@@ -8,12 +8,15 @@ from quangis import error
 from quangis.transformation.cct import cct
 
 print("Reading from standard input...")
+count = 0
 for line in stdin.readlines():
     line = line.strip()
     if line:
         try:
             expr = cct.parse(line)
-            print("SUCCESS: ", expr)
         except error.AlgebraTypeError as e:
+            count += 1
+            print()
             print("FAILED: ", line)
             print(e)
+print(count)
