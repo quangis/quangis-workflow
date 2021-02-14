689f77005879500243e1df72e85caeb2828ed5f3
Niels Steenbergen <n.steenbergen@uu.nl>
2021-02-14 13:16:25 +0100

    Rename Term to TypeTerm.

--- quangis/__init__.py
+++ quangis/__init__.py
@@ -1,5 +1,5 @@
 from quangis.taxonomy import Taxonomy
 from quangis.ontology import Ontology
 from quangis.transformation.algebra import TransformationAlgebra, Expr
-from quangis.transformation.type import TypeVar, TypeOperator, Term
+from quangis.transformation.type import TypeVar, TypeOperator, TypeTerm
 from quangis.transformation.cct import cct
