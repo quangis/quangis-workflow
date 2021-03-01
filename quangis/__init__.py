707a2d75dfcbd8ed3f696336239c12d9aa62ace3
Niels Steenbergen <n.steenbergen@uu.nl>
2021-03-01 16:41:09 +0100

    Remove re-exporting.
    Was just cluttering things.

--- quangis/__init__.py
+++
@@ -1,5 +0,0 @@
-from quangis.taxonomy import Taxonomy
-from quangis.ontology import Ontology
-from quangis.transformation.algebra import Expr
-from quangis.transformation.type import Operator
-from quangis.transformation.cct import cct
