6a01a67c3723b4b5b18cdfc8feb060cce421cbc7
Niels Steenbergen <n.steenbergen@uu.nl>
2021-04-19 18:10:29 +0200

    Add visual test.

---
+++ visual-test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Visually inspect parse trees for all tool expressions.
+"""
+import rdflib  # type: ignore
+from cct import cct
+
+TOOLS = rdflib.Namespace("http://geographicknowledge.de/vocab/GISTools.rdf#")
+path = "ToolDescription_TransformationAlgebra.ttl"
+g = rdflib.Graph()
+g.parse(path, format=rdflib.util.guess_format(path))
+for s, o in sorted(g.subject_objects(TOOLS.algebraexpression)):
+    e = cct.parse(o)
+    assert e
+    print(s)
+    if any(e.type.variables()):
+        print(e.tree())
+    else:
+        print(e.type)
+    print()
