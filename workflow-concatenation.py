d7541b85b976096b201726cc97194407d11ff833
Niels Steenbergen <n.steenbergen@uu.nl>
2021-06-25 22:40:33 +0200

    Find workflow expressions more carefully.
    Needs a lot of work.

--- workflow-concatenation.py
+++ workflow-concatenation.py
@@ -9,12 +9,13 @@ from __future__ import annotations
 import rdflib  # type: ignore
 from rdflib import Graph
 from rdflib.term import Node  # type: ignore
+from rdflib.plugins import sparql
 from glob import glob
 
-from transformation_algebra.expr import Expr, Variable
+from transformation_algebra.expr import Expr
 from cct import cct
 
-from typing import List, Tuple, Optional
+from typing import List, Tuple, Optional, Dict
 
 RDF = rdflib.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 RDFS = rdflib.Namespace("http://www.w3.org/2000/01/rdf-schema#")
@@ -30,95 +31,97 @@ def graph(*paths: str) -> rdflib.Graph:
     return g
 
 
-class Workflow(object):
-
-    tools = graph('workflows/ToolDescription_TransformationAlgebra.ttl')
-
-    def __init__(self, source: Graph, root: Node):
-        self.source: Graph = source
-        self.root: Node = root
-
-    def steps(self) -> List[Node]:
-        "Get steps of the workflow."
-        return list(self.source.objects(subject=self.root, predicate=WF.edge))
-
-    def resource_expr(self, resource: Node) -> Tuple[Optional[Node], Expr]:
-        "Get an algebra expression associated with a resource node."
-        # May loop infinitely
-        steps = list(self.source.subjects(predicate=WF.output, object=resource))
-        if steps:
-            # If there are any steps that produce the resource as output...
-            step = steps[0]
-            args = []
-            for i in (WF.input1, WF.input2, WF.input3):
-
-                input_resource = list(
-                    self.source.objects(subject=step, predicate=i))
-
-                if len(input_resource) > 0:
-                    _, expr1 = self.resource_expr(input_resource[0])
-                    args.append(expr1)
-
-            tool = next(self.source.objects(subject=step,
-                predicate=WF.applicationOf))
-
-            expr_str = [str(expr)
-                for expr in self.tools.objects(subject=tool,
-                    predicate=TOOLS.algebraexpression)
-            ][0]
-            expr = cct.parse(expr_str)
-            assert expr
-            return tool, expr.supply(*args)
-        else:
-            # Else, it is not the output of a step, so just a variable for now
-            return None, Variable()
-
-    def output(self) -> List[Node]:
-        "Find the output(s) of this workflow."
-        result = []
-        for step in self.steps():
-            outputs = list(
-                self.source.objects(subject=step, predicate=WF.output))
-            if outputs:
-                assert len(outputs) == 1
-                if all([(None, i, outputs[0]) not in self.source
-                        for i in (WF.input1, WF.input2, WF.input3)]):
-                    result.append(outputs[0])
-        return result
-
-    def expr(self) -> Tuple[Optional[Node], Expr]:
-        output_resource = self.output()[0]
-        return self.resource_expr(output_resource)
-
-    @staticmethod
-    def workflows(graph: Graph) -> List[Workflow]:
-        "Get workflows in an RDF graph."
-        return [
-            Workflow(graph, node)
-            for node in graph.subjects(predicate=RDF.type, object=WF.Workflow)]
-
-
 g = graph(
     "workflows/ToolDescription_TransformationAlgebra.ttl",
     *glob("workflows/**/*_cct.ttl"))
 
-result = g.query(
+
+"""
+A query to obtain all steps of a workflow and the relevant in- and outputs.
+"""
+query_steps = sparql.prepareQuery(
     """
-    SELECT ?wf WHERE {
+    SELECT
+        ?wf
+        ?tool
+        ?current_step_output ?is_final_output
+        ?expression
+        ?x1 ?x2 ?x3
+    WHERE {
         ?wf a wf:Workflow.
         ?wf wf:edge ?step.
-        ?step wf:output ?output.
-        ?output tools:applicationOf ?tool.
+        ?step wf:output ?current_step_output.
+
+        # What is the algebra expression for this step?
+        ?step wf:applicationOf ?tool.
+        ?tool tools:algebraexpression ?expression.
+
+        # What are the inputs?
+        OPTIONAL { ?step wf:input1 ?x1. }
+        OPTIONAL { ?step wf:input2 ?x2. }
+        OPTIONAL { ?step wf:input3 ?x3. }
+
+        # Is this the final step?
+        OPTIONAL {
+            { ?next_step wf:input1 ?current_step_output }
+            UNION
+            { ?next_step wf:input2 ?current_step_output }
+            UNION
+            { ?next_step wf:input3 ?current_step_output }.
+        }
+        BIND (!bound(?next_step) AS ?is_final_output).
     }
-    """, initNs={"wf": WF})
+    """,
+    initNs={"wf": WF, "tools": TOOLS}
+)
 
-for row in result:
-    print(row)
 
-# for fn in glob("workflows/**/*_cct.ttl"):
-#     print(fn)
-#     for wf in Workflow.workflows(graph(fn)):
-#         try:
-#             wf.expr()
-#         except Exception as e:
-#             print(e)
+def construct(g: Graph, workflow: Node) -> None:
+    """
+    Concatenate workflow expressions and add them to the graph.
+    """
+    description = next(g.objects(subject=workflow, predicate=RDFS.comment))
+
+    print()
+    print("Current workflow: ", description)
+
+    # Set of nodes representing source data
+    sources = set(g.objects(subject=workflow, predicate=WF.source))
+
+    # Map every step to its algebra expression and the source nodes/steps it
+    # gets its inputs from
+    step_info: Dict[Node, Tuple[Expr, List[Node]]] = dict()
+    output_node: Optional[Node] = None
+    steps = g.query(query_steps, initBindings={"wf": workflow})
+
+    for step in steps:
+        current = step.current_step_output
+
+        # Set the output node if we happen to chance upon it
+        if step.is_final_output:
+            assert not output_node, "multiple output nodes?"
+            output_node = current
+
+        expr = cct.parse(step.expression)
+        inputs = [x for x in (step.x1, step.x2, step.x3) if x]
+
+        step_info[current] = (expr, inputs)
+
+    assert output_node
+
+    stack = [output_node]
+    while stack:
+        current = stack.pop()
+        if current in step_info:
+            expr, inputs = step_info[current]
+            print(expr.type)
+            print(inputs)
+            stack.extend(inputs)
+        else:
+            assert current in sources, str(current)
+            print(current)
+
+
+# Produce all workflows
+for workflow in g.subjects(predicate=RDF.type, object=WF.Workflow):
+    construct(g, workflow)
