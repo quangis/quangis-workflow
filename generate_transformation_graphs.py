47c909013a1c5185c1c2d2f0524566861f807f47
Niels Steenbergen <n.steenbergen@uu.nl>
2021-10-15 20:32:00 +0200

    Encapsulate workflow graphs.

--- generate_transformation_graphs.py
+++ generate_transformation_graphs.py
@@ -7,51 +7,23 @@ the algebra expressions for each individual use of a tool.
 from __future__ import annotations
 
 import os.path
-from rdflib import Graph  # type: ignore
-from rdflib.namespace import RDF  # type: ignore
-from rdflib.term import Node  # type: ignore
 from glob import glob
 from sys import stderr
-
-from transformation_algebra.expr import Expr
 from transformation_algebra.rdf import TransformationGraph
 
 from cct import CCT, cct  # type: ignore
-from util import write_graph, graph, WF, TOOLS  # type: ignore
-
-
-def get_steps(workflows: Graph, tools: Graph, wf: Node
-        ) -> dict[Node, tuple[Expr, list[Node]]]:
-    """
-    Map the output node of each step to an expression and its input nodes.
-    """
-    steps = {}
-    for step in workflows.objects(wf, WF.edge):
-        tool = workflows.value(step, WF.applicationOf, any=False)
-        assert tool, "workflow has an edge without a tool"
-        expr = tools.value(tool, TOOLS.algebraexpression, any=False)
-        assert expr, f"{tool} has no algebra expression"
-        output = workflows.value(step, WF.output, any=False)
-        steps[output] = cct.parse(expr).primitive(), list(filter(bool, (
-            workflows.value(step, p) for p in (WF.input1, WF.input2, WF.input3)
-        )))
-    return steps
+from util import write_graph, graph  # type: ignore
+from workflow import Workflow
 
 
 tools = graph("TheoryofGISFunctions/ToolDescription_TransformationAlgebra.ttl")
 
 for workflow_file in glob("TheoryofGISFunctions/Scenarios/**/*_cct.ttl"):
-
-    print(f"\nWorkflow {workflow_file}", file=stderr)
-
-    workflow_graph = graph(workflow_file)
-    wf = workflow_graph.value(None, RDF.type, WF.Workflow, any=False)
-
     try:
-        sources = set(workflow_graph.objects(wf, WF.source))
-        steps = get_steps(workflow_graph, tools, wf)
+        print(f"\nWorkflow {workflow_file}", file=stderr)
+        workflow = Workflow(workflow_file, tools)
         g = TransformationGraph(cct, CCT)
-        g.add_workflow(wf, sources, steps)
+        g.add_workflow(workflow.root, workflow.sources, workflow.format())
     except Exception as e:
         print("Failure: ", e, file=stderr)
     else:
---
+++ workflow.py
@@ -0,0 +1,48 @@
+"""
+Workflow graph class that's easier to work with.
+"""
+
+from rdflib import Graph
+from rdflib.namespace import RDF, RDFS
+from rdflib.term import Node, URIRef
+import rdflib.util
+
+from transformation_algebra.expr import Expr
+
+from cct import cct
+from util import WF, TOOLS
+
+
+class Workflow(Graph):
+    def __init__(self, fn: str, tools: Graph):
+        super().__init__()
+        self.parse(fn, format=rdflib.util.guess_format(fn))
+
+        self.file = fn
+        self.root = self.value(None, RDF.type, WF.Workflow, any=False)
+        self.description = self.value(self.root, RDFS.comment)
+        self.steps = set(self.objects(self.root, WF.edge))
+        self.sources = set(self.objects(self.root, WF.source))
+
+        # map output nodes to input nodes, tools and expressions
+        self.inputs: dict[Node, list[Node]] = {}
+        self.tools: dict[Node, URIRef] = {}
+        self.expressions: dict[Node, str] = {}
+        for step in self.steps:
+            out = self.value(step, WF.output, any=False)
+            self.tools[out] = tool = self.value(
+                step, WF.applicationOf, any=False)
+            self.expressions[out] = expr = tools.value(
+                tool, TOOLS.algebraexpression, any=False)
+            self.inputs[out] = list(filter(bool, (
+                self.value(step, p) for p in (WF.input1, WF.input2, WF.input3)
+            )))
+
+            assert tool, "workflow has an edge without a tool"
+            assert expr, f"{tool} has no algebra expression"
+
+    def format(self) -> dict[Node, tuple[Expr, list[Node]]]:
+        return {
+            output: (cct.parse(self.expressions[output]).primitive(), inputs)
+            for output, inputs in self.inputs.items()
+        }
