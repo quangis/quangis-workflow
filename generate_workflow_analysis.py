b464e598257e9a5d265082d6483d18cef02adb2f
Niels Steenbergen <n.steenbergen@uu.nl>
2021-10-24 15:40:54 +0200

    Generate analysis files.

--- generate_workflow_analysis.py
+++ generate_workflow_analysis.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 import os.path
 from glob import glob
 from sys import stderr
-from transformation_algebra.rdf import TransformationGraph
+from transformation_algebra import TransformationGraph
 
 from cct import CCT, cct  # type: ignore
 from util import graph, Labeller  # type: ignore
@@ -19,19 +19,23 @@ from workflow import Workflow
 tools = graph("TheoryofGISFunctions/ToolDescription_TransformationAlgebra.ttl")
 
 for workflow_file in glob("TheoryofGISFunctions/Scenarios/**/*_cct.ttl"):
-    workflow = Workflow(workflow_file, tools)
-    name = os.path.splitext(os.path.basename(workflow_file))[0]
-
-    label = Labeller()
-    print("".join('*' for _ in range(79)))
-    print(workflow.file)
-    print("SOURCES:", [label[i] for i in workflow.sources])
-    print("".join('.' for _ in range(79)))
-    for out in workflow.outputs:
-        print("TOOL:", workflow.tools[out])
-        print("OUTPUT:", label[out])
-        print("INPUTS:", [label[i] for i in workflow.inputs[out]])
-        print("EXPRESSION:", workflow.expressions[out])
-        print("PRIMITIVE:",
-            cct.parse(workflow.expressions[out]).primitive())
-        print()
+    try:
+        workflow = Workflow(workflow_file, tools)
+        name = os.path.splitext(os.path.basename(workflow_file))[0]
+        print(name)
+        label = Labeller()
+        with open(f"build/{name}.txt", 'w') as f:
+            print("SOURCES:", [label[i] for i in workflow.sources], file=f)
+            for out in workflow.outputs:
+                print(file=f)
+                print("TOOL:", workflow.tools[out], file=f)
+                print("OUTPUT:", label[out], file=f)
+                print("INPUTS:", [label[i] for i in workflow.inputs[out]], file=f)
+                print("EXPRESSION:", workflow.expressions[out], file=f)
+                print("PRIMITIVE:",
+                    cct.parse(workflow.expressions[out]).primitive(), file=f)
+    except Exception as e:
+        print("Failure: ", e)
+    else:
+        print("Success.")
+    print()
