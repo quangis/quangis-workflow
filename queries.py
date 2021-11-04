3e94cf7a1c6b4b7f06bd4627dd5aa67f1817e7f4
Niels Steenbergen <n.steenbergen@uu.nl>
2021-11-04 11:33:46 +0100

    `TransformationQuery` no longer takes `*nargs`.

--- queries.py
+++ queries.py
@@ -14,92 +14,92 @@ groupby = groupbyR  # Temporary solution
 dist = nDist  # Temporary solution
 
 # 1. (Noise) What is the proportion of noise ≥ 70dB in Amsterdam?
-noiseProPortionAmsterdam = TransformationQuery(
+noiseProPortionAmsterdam = TransformationQuery((
     R3a(Obj, Reg, Ratio), ..., apply, [
         (..., ratio),
         (..., groupby, [(..., size), (..., pi1, R2(Loc, _))]),
         (..., apply, [(..., size), (..., R2(Obj, Reg))])
-    ],
+    ]),
     namespace=CCT
 )
 
 # 2. (Population) What is the number of inhabitants for each neighbourhood in
 # Utrecht?
-# amountObjectsUtrecht = TransformationQuery(
+# amountObjectsUtrecht = TransformationQuery((
 #     R3a(Obj, Reg, Count), Choice(
 #         (groupby, [(..., sum), (..., R2(Reg, Count))]),
 #         (groupby, [(..., count), (..., R2(Obj, Reg))])
-#     ),
+#     )),
 #     namespace=CCT
 # )
 
 # 3. (Temperature) What is the average temperature for each neighbourhood in
 # Utrecht?
-amountFieldUtrecht = TransformationQuery(
+amountFieldUtrecht = TransformationQuery((
     R3a(Obj, Reg, Itv), ..., groupby, [
         (..., avg),
         (..., R2(Loc, Itv))
-    ],
+    ]),
     namespace=CCT
 )
 
 # 4. What is the travel distance to the nearest hospital in California?
-hospital_accessibility = TransformationQuery(
+hospital_accessibility = TransformationQuery((
     R3a(Obj, Reg, Ratio), ..., groupby, [
         (..., min),
         (..., nDist),
         (..., R2(Obj, Reg))
-    ],
+    ]),
     namespace=CCT
 )
 
 # 5. What is the impact of roads on deforestation in the Amazon rainforest?
-# deforestation = TransformationQuery(
+# deforestation = TransformationQuery((
 #     R2(Bool, Ratio), [
 #         (..., size, pi1, ..., extrapol, R2(Obj, Reg)),
 #         (..., size, pi1, ..., R2(Loc, _))
-#     ],
+#     ]),
 #     namespace=CCT
 # )
 
 # 6. What is the potential of solar power for each rooftop in the Glover Park
 # neighbourhood in Washington DC?
-solar = TransformationQuery(
+solar = TransformationQuery((
     R3a(Obj, Reg, Ratio), ..., groupby, [
         (..., avg),
         (..., R2(Loc, Ratio)),
         (..., R2(Obj, Reg))
-    ],
+    ]),
     namespace=CCT
 )
 
 # 7. (Road-Access) What is the percentage of rural population within 2km
 # distance to all-season roads in Shikoku, Japan?
-# infrastructureAccess = TransformationQuery(
+# infrastructureAccess = TransformationQuery((
 #     R2(Reg, Ratio), [
 #         (..., ratio),
 #         (..., R2(Reg, Ratio), ..., [
 #             (..., extrapol, R2(Obj, Reg)),
 #             (..., R2(Reg, Count))
 #         ])
-#     ],
+#     ]),
 #     namespace=CCT
 # )
 
 # 8. (Water-risk) Which urban areas are at risk from water depletion in
 # Ogallala (High Plains) Aquifer, US?
-# aquifer = TransformationQuery(
+# aquifer = TransformationQuery((
 #     R3a(Obj, Reg, Nom), ..., [
 #         (..., select, ..., loTopo, [
 #             (..., pi1, ..., R2(Loc, Nom)),
 #             (..., select, ..., oDist, ..., R2(Obj, Reg))
 #         ])
-#     ],
+#     ]),
 #     namespace=CCT
 # )
 
 # 9. What is the stream runoff during a predicted rainstorm in Vermont, US?
-floods = TransformationQuery(
+floods = TransformationQuery((
     R2(Ord, Ratio), ..., groupby, [
         (..., size),
         (..., R2(Loc, Ord), ..., groupby, [
@@ -107,13 +107,13 @@ floods = TransformationQuery(
             (..., accumulate, ..., flowdirgraph, ..., R2(Loc, Itv)),
             (..., lgDist)
         ])
-    ],
+    ]),
     namespace=CCT
 )
 
 # 10. What is the malaria incidence rate per 1000 inhabitants in the Democratic
 # Republic of the Congo?
-malaria = TransformationQuery(
+malaria = TransformationQuery((
     R3a(Obj, Reg, Ratio), [
         (..., ratio),
         (..., R2(Obj, Count)),
@@ -121,6 +121,6 @@ malaria = TransformationQuery(
             (..., sum),
             (..., R2(Reg, Count))
         ])
-    ],
+    ]),
     namespace=CCT
 )
