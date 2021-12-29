143e7f84f0fce496ebeaea8753bbc183c3114d35
Niels Steenbergen <n.steenbergen@uu.nl>
2021-12-29 21:38:46 +0100

    Add stylized results for queries.

--- utils/run_queries.py
+++ utils/run_queries.py
@@ -8,17 +8,18 @@ import importlib.machinery
 import importlib.util
 from rdflib import Graph  # type: ignore
 from transformation_algebra.query import Query
+from typing import Iterator
+from rdflib.term import Node
 
 from config import query_paths, build_path  # type: ignore
 
 
-def extract_queries() -> dict[tuple[str, str], Query]:
+def extract_queries() -> Iterator[tuple[str, str, set[Node], Query]]:
     """
     Extract queries defined in modules.
     """
     # Perhaps overengineered but makes it simple to add and change queries in
     # dedicated modules
-    result: dict[tuple[str, str], Query] = dict()
     for path in query_paths:
         name = path.stem
         loader = importlib.machinery.SourceFileLoader(name, str(path))
@@ -26,47 +27,57 @@ def extract_queries() -> dict[tuple[str, str], Query]:
         assert spec
         module = importlib.util.module_from_spec(spec)
         loader.exec_module(module)
+
+        workflows = getattr(module, 'workflows')
         for variant in dir(module):
             query = getattr(module, variant)
             if isinstance(query, Query):
-                result[name, variant] = query
-    return result
+                yield name, variant, workflows, query
+
+
+def result(actual: set, expected: set,
+        correct=lambda x: f"\033[32m{x}\033[0m",
+        wrong=lambda x: f"\033[31m{x}\033[0m",
+        empty='none') -> str:
+    """
+    Stylize the contents of a set depending on whether its elements should be
+    there or not.
+    """
+    if actual:
+        return ", ".join(correct(r) if r in expected else wrong(r)
+            for r in actual)
+    else:
+        return (correct if not expected else wrong)(empty)
 
 
 wfgraph = Graph(store='SPARQLStore')
 wfgraph.open("http://localhost:3030/name")
 
-try:
-    if wfgraph:
-        pass
-except ValueError:
-    print("There was an issue with the graph store; not running the queries")
-    wfgraph = None
-
-
-for (scenario, variant), query in extract_queries().items():
+for scenario, variant, expected_workflows, query in extract_queries():
     if variant != "query":
         continue
-    print('\033[1m', scenario, '\033[0m', variant)
+
+    print(f'\033[1m\033[4m{scenario}\033[0m', variant)
 
     # Print query to file for inspection
     path = build_path / f"{scenario}_{variant}.rq"
-    print("Building", path.name)
+    # print("Building", path.name)
     with open(path, 'w') as f:
         f.write(query.sparql())
 
-    if wfgraph:
-        for i, line in enumerate(wfgraph.query(query.sparql()), start=1):
-            print(i, line.description)
+    try:
+        results = wfgraph.query(query.sparql())
+        positives = set(r.workflow for r in results)
 
-# result = graph.query("""
-#     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-#     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-#     PREFIX ta: <https://github.com/quangis/transformation-algebra#>
-#     PREFIX cct: <https://github.com/quangis/cct#>
-#     SELECT ?s WHERE {?s rdf:type ta:Transformation. }
-# """)
+        false_pos = (positives - expected_workflows)
+        false_neg = (expected_workflows - positives)
+        correct = positives - false_pos - false_neg
 
-# for line in result:
-#     print(line)
+        print("Correct:", result(correct, expected_workflows))
+        print("False positives:", result(false_pos, set()))
+        print("Missing:", result(false_neg, set()))
 
+    except ValueError:
+        print("Not firing queries, since the server is down.")
+
+    print()
