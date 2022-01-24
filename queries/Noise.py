2fed180de2762a1df459c7642df9295163fd9814
Niels Steenbergen <n.steenbergen@uu.nl>
2022-01-24 14:02:18 +0100

    Revert previous.

--- queries/Noise.py
+++ queries/Noise.py
@@ -3,7 +3,7 @@
 
 from config import REPO  # type: ignore
 from transformation_algebra.type import _
-from transformation_algebra.query import Query, AND, IMMEDIATELY
+from transformation_algebra.query import Query, AND, STEPS
 from cct import cct, R3a, R2, Obj, Reg, Nom, Ratio, Ord, Loc, apply, ratio, groupbyLR, size, pi1  # type: ignore
 
 workflows = {
@@ -14,7 +14,7 @@ workflows = {
 query = Query(cct,
     [R3a(Obj, Reg, Ratio), apply, AND(
         ratio,
-        [groupbyLR, AND(size, IMMEDIATELY(pi1, R2(Loc, _)))],
+        [groupbyLR, AND(size, STEPS(pi1, R2(Loc, _)))],
         [apply, AND(size, R2(Obj, Reg))]
     )]
 )
