e8601a6330de3fd121653fd991a623b24de461a7
Niels Steenbergen <n.steenbergen@uu.nl>
2022-02-20 21:00:17 +0100

    CSV for normal queries.

--- utils/run_queries.py
+++ utils/run_queries.py
@@ -8,54 +8,13 @@
 
 from __future__ import annotations
 
-import importlib.machinery
-import importlib.util
 from rdflib import Graph  # type: ignore
-from transformation_algebra.query import Query
-from typing import Iterator
-from rdflib.term import Node
-from openpyxl import Workbook
-from openpyxl.formatting import Rule
-from openpyxl.styles import Font, PatternFill, Border
-from openpyxl.styles.differential import DifferentialStyle
+from rdflib.term import Node  # type: ignore
+import csv
 
-from config import query_paths, build_path, REPO  # type: ignore
+from config import build_path, REPO  # type: ignore
 
-
-def extract_queries() -> Iterator[tuple[str, str, set[Node], Query]]:
-    """
-    Extract queries defined in modules.
-    """
-    # Perhaps overengineered but makes it simple to add and change queries in
-    # dedicated modules
-    for path in query_paths:
-        name = path.stem
-        loader = importlib.machinery.SourceFileLoader(name, str(path))
-        spec = importlib.util.spec_from_loader(name, loader)
-        assert spec
-        module = importlib.util.module_from_spec(spec)
-        loader.exec_module(module)
-
-        workflows = getattr(module, 'workflows')
-        for variant in dir(module):
-            query = getattr(module, variant)
-            if isinstance(query, Query):
-                yield name, variant, workflows, query
-
-
-def result(actual: set, expected: set,
-        correct=lambda x: f"\033[32m{x}\033[0m",
-        wrong=lambda x: f"\033[31m{x}\033[0m",
-        empty='none') -> str:
-    """
-    Stylize the contents of a set depending on whether its elements should be
-    there or not.
-    """
-    if actual:
-        return ", ".join(correct(r) if r in expected else wrong(r)
-            for r in actual)
-    else:
-        return (correct if not expected else wrong)(empty)
+from queries import all_queries, all_workflows
 
 
 wfgraph = Graph(store='SPARQLStore')
@@ -67,75 +26,77 @@ option_variants = {
     "types": {"by_types": True, "by_chronology": False}
 }
 
-queries = list(
-    (scenario, variant, expected, query)
-    for scenario, variant, expected, query in extract_queries()
-)
-all_workflows: list[Node] = list(
-    set.union(*(expected for (_, _, expected, _) in queries))
-)
 
-wb = Workbook()
-header = ["Scenario", "Variant", "Options"] + [
-    str(wf)[len(REPO):] for wf in all_workflows]
+def wfname(wf: Node) -> str:
+    return str(wf)[len(REPO):]
 
-sheet_actual = wb.active
-sheet_actual.title = "Actual"
-sheet_actual.append(header)
 
-font_wrong = Font(color="FF0000")
-row = 1
+header = ["Scenario", "Variant", "Options"] + sorted([
+    wfname(wf) for wf in all_workflows])
 
-for scenario, variant, expected_workflows, query in queries:
+with open(build_path / "results.csv", 'w', newline='') as f:
 
-    if not variant.startswith("eval"):
-        continue
+    w = csv.DictWriter(f, fieldnames=header)
+    w.writeheader()
 
-    for optname, options in option_variants.items():
+    # for scenario, variant, expected_workflows, query in all_queries:
+    for elem in all_queries:
+        scenario = elem["name"]
+        expected = set(elem["expected"])
+        for variant, query in elem["variants"].items():
 
-        print(f'\033[1m\033[4m{scenario}\033[0m', variant, optname)
+            if not variant.startswith("eval"):
+                continue
 
-        sparql = query.sparql(**options)
+            for optname, options in option_variants.items():
 
-        # Print query to file for inspection
-        path = build_path / f"{scenario}_{variant}_{optname}.rq"
-        # print("Building", path.name)
-        with open(path, 'w') as f:
-            f.write(sparql)
+                result: dict[str, str] = {
+                    "Scenario": scenario,
+                    "Variant": variant,
+                    "Options": optname
+                }
 
-        # Run the query
-        try:
-            results = wfgraph.query(sparql)
-            positives = set(r.workflow for r in results)
+                print(f'\033[1m\033[4m{scenario}\033[0m', variant, optname)
 
-            # Write to spreadsheet
-            row += 1
-            sheet_actual.append([scenario, variant, optname])
-            for col, wf in enumerate(all_workflows, start=4):
-                cell = sheet_actual.cell(row, col, "1" if wf in positives else "0")
-                if (wf in positives) ^ (wf in expected_workflows):
-                    cell.font = font_wrong
+                sparql = query.sparql(**options)
 
-            # Write to terminal
-            false_pos = (positives - expected_workflows)
-            false_neg = (expected_workflows - positives)
-            correct = positives - false_pos - false_neg
+                # Print query to file for inspection
+                path = build_path / f"{scenario}_{variant}_{optname}.rq"
+                # print("Building", path.name)
+                with open(path, 'w') as f:
+                    f.write(sparql)
 
-            print("Correct:", result(correct, expected_workflows))
-            print("False positives:", result(false_pos, set()))
-            print("Missing:", result(false_neg, set()))
+                # Run the query
+                try:
+                    results = wfgraph.query(sparql)
+                    pos = set(r.workflow for r in results)
 
-        except ValueError:
-            print("Not firing queries, since the server is down.")
+                    for wf in all_workflows:
+                        if wf in pos:
+                            if wf in expected:
+                                s = "TP"
+                            else:
+                                s = "FP"
+                        else:
+                            if wf in expected:
+                                s = "FN"
+                            else:
+                                s = "TN"
 
-        print()
+                        result[wfname(wf)] = s
 
-sheet_expected = wb.create_sheet(title="Expected")
-sheet_expected.append(header)
-for scenario, variant, expected_workflows, _ in queries:
-    for optname in option_variants.keys():
-        sheet_expected.append([scenario, variant, optname] + [
-            "1" if wf in expected_workflows else "0" for wf in all_workflows
-        ])
+                    w.writerow(result)
 
-wb.save(filename=build_path / "results.xlsx")
+                    # Write to terminal
+                    false_pos = (pos - expected)
+                    false_neg = (expected - pos)
+                    correct = pos - false_pos - false_neg
+
+                    print("Correct:", correct)
+                    print("False positives:", false_pos)
+                    print("Missing:", false_neg)
+
+                except ValueError:
+                    print("Not firing queries, since the server is down or timed out.")
+
+                print()
