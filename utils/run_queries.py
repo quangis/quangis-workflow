7a5b40844e559993aabfe25033514627402c9762
Niels Steenbergen <n.steenbergen@uu.nl>
2022-02-23 16:21:07 +0100

    Add just-output variant to queries.

--- utils/run_queries.py
+++ utils/run_queries.py
@@ -22,8 +22,9 @@ wfgraph.open("http://localhost:3030/name")
 
 # Varying options to try
 option_variants = {
-    "ordered": {"by_types": True, "by_chronology": True},
-    "types": {"by_types": True, "by_chronology": False}
+    "output": {"by_operators": False, "by_types": False, "by_chronology": False},
+    "types": {"by_operators": False, "by_types": True, "by_chronology": False},
+    "ordered": {"by_operators": False, "by_types": True, "by_chronology": True}
 }
 
 
