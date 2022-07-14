4d2398916f748e16b049a11e508734840006c2d9
Niels Steenbergen <n.steenbergen@uu.nl>
2022-07-14 16:18:16 +0200

    Merge workflow.py and ta-tool.py
    These should be packaged up in the end and made independent from this
    repository.

--- utils/ta-tool.py
+++ utils/ta-tool.py
@@ -4,17 +4,69 @@ from __future__ import annotations
 
 import csv
 from rdflib import Graph  # type: ignore
-from rdflib.term import Node, Literal  # type: ignore
-from rdflib.namespace import RDFS  # type: ignore
+from rdflib.term import Node, URIRef, Literal  # type: ignore
+from rdflib.namespace import RDF, RDFS  # type: ignore
 from rdflib.tools.rdf2dot import rdf2dot  # type: ignore
+from pathlib import Path
 from plumbum import cli  # type: ignore
 from itertools import chain
 from transformation_algebra import TransformationQuery, TransformationGraph, TA
 from typing import NamedTuple, Iterable
 
-from config import REPO, TOOLS  # type: ignore
-from workflow import Workflow  # type: ignore
+from config import tools_path, WF, REPO, TOOLS  # type: ignore
 from cct import cct
+
+
+tools: Graph = Graph()
+tools.parse(tools_path, format='ttl')
+
+
+class Workflow(Graph):
+    def __init__(self, path: Path):
+        super().__init__()
+        self.parse(path, format='ttl')
+
+        self.path = path
+        self.root = self.value(None, RDF.type, WF.Workflow, any=False)
+        self.description = self.value(self.root, RDFS.comment)
+        self.steps = set(self.objects(self.root, WF.edge))
+        self.sources = set(self.objects(self.root, WF.source))
+
+        # map output nodes to input nodes, tools and expressions
+        self.output: Node
+        self.outputs: set[Node] = set()
+        self.inputs: dict[Node, list[Node]] = {}
+        self.tools: dict[Node, URIRef] = {}
+        self.expressions: dict[Node, str] = {}
+        self.comment: dict[Node, str] = {}
+
+        self.wf: dict[Node, tuple[str, list[Node]]] = {}
+
+        for step in self.steps:
+            out = self.value(step, WF.output, any=False)
+            self.outputs.add(out)
+
+            self.tools[out] = tool = self.value(
+                step, WF.applicationOf, any=False)
+            assert tool, "workflow has an edge without a tool"
+
+            self.expressions[out] = expr = tools.value(
+                tool, cct.namespace.expression, any=False)
+            assert expr, f"{tool} has no algebra expression"
+
+            if comment := self.value(step, RDFS.comment):
+                self.comment[out] = comment
+
+            self.inputs[out] = [node
+                for pred in (WF.input1, WF.input2, WF.input3)
+                if (node := self.value(step, pred))
+            ]
+
+            self.wf[out] = expr, self.inputs[out]
+
+        final_outputs = (self.outputs - set(chain.from_iterable(self.inputs.values())))
+        assert len(final_outputs) == 1
+        self.output, = final_outputs
 
 
 class Tatool(cli.Application):
