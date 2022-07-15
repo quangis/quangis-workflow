8eca5253939482055f3811c2f098ff7af85a3703
Niels Steenbergen <n.steenbergen@uu.nl>
2022-07-15 13:19:50 +0200

    Unittest being convoluted.

--- test.py
+++ test.py
@@ -1,43 +1,67 @@
 #!/usr/bin/env python3
 from __future__ import annotations
 import unittest
-from rdflib import Graph, RDF, Namespace
+from rdflib import Graph, RDF, RDFS, Namespace
 from rdflib.term import Node
 from pathlib import Path
+from typing import Iterable
 
 WF = Namespace('http://geographicknowledge.de/vocab/Workflow.rdf#')
 root_path = Path(__file__).parent
 
 
-def testWorkflows(path: Path) -> unittest.TestSuite:
-    wf = path.stem
-    graph = Graph()
-    graph.parse(path)
-    root = graph.value(None, RDF.type, WF.Workflow, any=False)
-    assert root
-
-    class TestWorkflow1(unittest.TestCase):
-        def setUp(self):
-            self.wf = wf
-            self.graph = graph
-            self.root = root
-
-        def test_every_node_has_a_tool(self):
-            """
-            Every node (indicated by wf:edge) in a workflow should have a
-            tool (indicated by wf:applicationOf).
-            """
-            with self.subTest(name=self.wf):
-                for step in self.graph.objects(self.root, WF.edge):
-                    self.assertIsNone(self.graph.value(step,
-                        WF.applicationOf, any=False))
-
+def parameterized(params: Iterable):
+    "Turn a test case into a suite of parameterized test cases."
     testloader = unittest.TestLoader()
-    testnames = testloader.getTestCaseNames(TestWorkflow1)
-    suite = unittest.TestSuite()
-    for name in testnames:
-        suite.addTest(TestWorkflow1(name))
-    return suite
+
+    def decorator(cls: type) -> unittest.TestSuite:
+        suite = unittest.TestSuite()
+        for testname in testloader.getTestCaseNames(cls):
+            for param in params:
+                suite.addTest(cls(testname, param))
+        return suite
+
+    return decorator
+
+
+    # for param in params:
+    #     wf = path.stem
+    #     graph = Graph()
+    #     graph.parse(path)
+    #     root = graph.value(None, RDF.type, WF.Workflow, any=False)
+    #     assert root
+
+    #     class TestWorkflow1(cls):
+    #         def setUp(self):
+    #             self.wf = wf
+    #             self.graph = graph
+    #             self.root = root
+
+    #     testnames = testloader.getTestCaseNames(cls)
+    #     for name in testnames:
+    #         suite.addTest(TestWorkflow1(name))
+    # return suite
+
+
+@parameterized(root_path.glob("workflows/*.ttl"))
+class TestWorkflow(unittest.TestCase):
+    def __init__(self, runMethod: str, path: Path):
+        super().__init__(runMethod)
+        self.wf = path.stem
+        self.graph = Graph()
+        self.graph.parse(path)
+        self.root = self.graph.value(None, RDF.type, WF.Workflow, any=False)
+
+    def test_every_node_has_an_existing_tool(self):
+        # Every node (indicated by wf:edge) in a workflow should have a tool
+        # (indicated by wf:applicationOf). Each of those tools must exist in
+        # the tool ontology.
+        for step in self.graph.objects(self.root, WF.edge):
+            label = str(self.graph.value(step, RDFS.comment, any=False))
+            with self.subTest(step_label=label):
+                self.assertIsNone(self.graph.value(step,
+                    WF.applicationOf, any=False))
+                # TODO must exist
 
 
 class TestTools(unittest.TestCase):
@@ -61,8 +85,6 @@ class TestTools(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    # unittest.main()
     runner = unittest.TextTestRunner(verbosity=3)
-    suite = unittest.TestSuite()
-    for path in root_path.glob("workflows/*.ttl"):
-        suite.addTest(testWorkflows(path))
-    result = runner.run(suite)
+    result = runner.run(TestWorkflow)
