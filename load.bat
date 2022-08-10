0121b82bbd8c61ebc7651be6bcfb9ecc6e2d5c21
Niels Steenbergen <n.steenbergen@uu.nl>
2022-08-10 14:59:09 +0200

    Update to working bat file.
    Didn't check before because Windows

--- load.bat
+++ load.bat
@@ -1,10 +1,3 @@
-python -m transformation_algebra vocab ^
-    -L D:\repositories\cct\language.py ^
-    -b marklogic -s https://qanda.soliscom.uu.nl:8000/
-
-for %%v in (D:\repositories\cct\workflows\*.ttl) do ^
-    python -m transformation_algebra graph ^
-    -L D:\repositories\cct\language.py ^
-    -T D:\repositories\cct\tools\tools.ttl ^
-    -b marklogic -s https://qanda.soliscom.uu.nl:8000/ ^
-    "%%v"
+for %%v in (D:\repositories\cct\workflows\*.ttl) do (
+    python -m transformation_algebra graph -L D:\repositories\cct\cct\language.py -T D:\repositories\cct\tools\tools.ttl -b marklogic -s "https://qanda.soliscom.uu.nl:8000" -u "<user>:<pass>" "%%v"
+)
