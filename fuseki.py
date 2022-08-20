9cd31793395e69f8b8484538fc9db32a99dde0ea
Niels Steenbergen <n.steenbergen@uu.nl>
2022-08-20 13:50:56 +0200

    Do all evaluation in Python script.

--- fuseki.py
+++ fuseki.py
@@ -5,15 +5,21 @@
 from __future__ import annotations
 
 import sys
+import csv
 import subprocess
 import requests
 import tarfile
 import shutil
 import threading
+from itertools import chain
 from pathlib import Path
+from rdflib.term import Node
 from typing import ContextManager
 
-from transformation_algebra.util.common import graph, build, to_store
+from transformation_algebra import TransformationQuery
+from transformation_algebra.namespace import TA, REPO
+from transformation_algebra.util.store import TransformationStore
+from transformation_algebra.util.common import (graph, build_transformation)
 from cct.language import cct
 
 ROOT = Path(__file__).parent
@@ -28,11 +34,11 @@ FUSEKI_JAR = FUSEKI_DIR / FUSEKI_NAME / "fuseki-server.jar"
 
 
 class FusekiServer(ContextManager):
-    def __init__(self, dataset: str, *args):
+    def __init__(self, dataset: str = "test_dataset"):
         self.process: (subprocess.Popen | None) = None
+        self.url = f"http://localhost:3030/{dataset}"
         self.ready = False
         self.dataset = dataset
-        self.args = list(args)
         self.prepare_executable()
 
     def __enter__(self):
@@ -93,13 +99,81 @@ class FusekiServer(ContextManager):
         assert FUSEKI_JAR.is_file()
 
 
+def summary_csv(path: Path | str,
+        table_expected: dict[str, set[Node]],
+        table_actual: dict[str, set[Node]]) -> None:
+
+    tasks = set(table_expected.keys())
+    workflows = set.union(*chain(table_expected.values(),
+        table_actual.values()))
+
+    header = ["Task", "Precision", "Recall"] + sorted([
+        str(wf)[len(REPO):] for wf in workflows])
+
+    with open(path, 'w', newline='') as h:
+        n_tpos, n_tneg, n_fpos, n_fneg = 0, 0, 0, 0
+        w = csv.DictWriter(h, fieldnames=header)
+        w.writeheader()
+        for task in sorted(tasks):
+            row: dict[str, str] = {"Task": task}
+
+            expected, actual = table_expected[task], table_actual[task]
+
+            for wf in workflows:
+                s = ("●" if wf in actual else "○")
+                s += ("⨯" if (wf in actual) ^ (wf in expected) else "")
+                row[str(wf)[len(REPO):]] = s
+
+            n_fpos += len(actual - expected)
+            n_fneg += len(expected - actual)
+            n_tpos += len(actual.intersection(expected))
+            n_tneg += len(workflows - expected - actual)
+            w.writerow(row)
+        try:
+            w.writerow({
+                "Precision": "{0:.3f}".format(n_tpos / (n_tpos + n_fpos)),
+                "Recall": "{0:.3f}".format(n_tpos / (n_tpos + n_fneg))
+            })
+        except ZeroDivisionError:
+            w.writerow({"Precision": "?", "Recall": "?"})
+
+
 if __name__ == '__main__':
     tools = graph(ROOT / "tools" / "tools.ttl")
-    with FusekiServer("cct", *sys.argv[1:]) as server:
 
-        # Build transformation graphs for every workflow
+    with FusekiServer() as server:
+
+        # Connect to Fuseki as client
+        store = TransformationStore.backend("fuseki", server.url)
+
+        # Build & send transformation graphs for every workflow
         for wf_path in ROOT.glob("workflows/*.ttl"):
-            print(f"Building transformation graph for {wf_path}...")
-            g = build(language=cct, tools=tools, workflow=graph(wf_path))
+            workflow = graph(wf_path)
+            print(f"Building transformation graph for workflow {wf_path}...")
+            g = build_transformation(cct, tools, workflow)
+            # print(g.serialize(format="ttl"))
             print("Sending transformation graph to Fuseki...")
-            to_store(g, backend="fuseki", url="http://localhost:3030/cct")
+            store.put(g)
+
+        # Build & fire query for every task
+        actual: dict[str, set[Node]] = dict()
+        expected: dict[str, set[Node]] = dict()
+        for task_path in ROOT.glob("tasks/*.ttl"):
+            print(f"Reading transformation graph for task {task_path}...")
+            name = task_path.stem
+            task_graph = graph(task_path)
+            query = TransformationQuery(cct, task_graph,
+                by_io=False,
+                by_types=False,
+                by_chronology=False,
+                unfold_tree=False)
+            expected[name] = set(task_graph.objects(query.root,
+                TA.implementation))
+
+            print("Querying Fuseki...")
+            actual[name] = result = store.query(query)
+            print(result)
+
+            # print(f"Results: {', '.join(str(wf) for wf in actual[name])}")
+
+        summary_csv("summary.csv", expected, actual)
