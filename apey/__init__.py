b9cb9138c1c4908cdd9416af31d01002e28f547d
Niels Steenbergen <n.steenbergen@uu.nl>
2022-10-01 15:42:35 +0200

    Remove packaging.
    I don't want to deal with maintaining a general-purpose Python interface
    to APE, because to do it properly, I'd have to address all APE features
    with Pythonic structures, and I'd have to output Workflow structures
    that aren't so particular to our project. I'll merge the single `ape.py`
    file back into the <https://github.com/quangis/workflow-synthesis>
    repository. It should be enough for anyone looking into how to interface
    with APE to get a headstart.

--- apey/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-This is a wrapper to APE, the Java application that is used for workflow
-synthesis. It interfaces with a JVM.
-"""
-
-from apey.ape import APE, Workflow, ToolsDict  # ignore: F401
