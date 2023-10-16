3de41cb594c8bbfbf03396991a70bdc77ad23eaa
Niels Steenbergen <n.steenbergen@uu.nl>
2023-10-16 13:11:32 +0200

    Deterministic `cct2ccd`
    Whenever a catch-all `_` occurs, we must first have tried other options.

--- quangis/cct2ccd.py
+++ quangis/cct2ccd.py
@@ -8,51 +8,51 @@ from rdflib import URIRef
 from transforge.type import TypeOperation, Product
 
 # Conversion from CCT to CCD as made by Simon
-conversion = {
-    "R(_,Count)": {CCD.NominalA: [CCD.CountA]},
-    "Reg": {CCD.LayerA: [CCD.RegionA]},
-    "C(Loc)": {CCD.LayerA: [CCD.RegionA]},
-    "R(_,Itv)": {CCD.NominalA: [CCD.IntervalA]},
-    "R(Loc,Bool)": {CCD.CoreConceptQ: [CCD.FieldQ], CCD.NominalA: [CCD.BooleanA]},
-    "R(Loc,Itv)": {CCD.CoreConceptQ: [CCD.FieldQ], CCD.NominalA: [CCD.IntervalA]},
-    "R(Loc,Nom)": {CCD.CoreConceptQ: [CCD.FieldQ], CCD.NominalA: [CCD.NominalA]},
-    "R(Loc,Ord)": {CCD.CoreConceptQ: [CCD.FieldQ], CCD.NominalA: [CCD.OrdinalA]},
-    "R(Loc,Ratio)": {CCD.CoreConceptQ: [CCD.FieldQ], CCD.NominalA: [CCD.RatioA]},
-    "R(_,Nom)": {CCD.NominalA: [CCD.NominalA]},
-    "R(Obj,_)": {CCD.CoreConceptQ: [CCD.ObjectQ]},
-    "R(Obj*Obj,Itv)": {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.NominalA: [CCD.IntervalA]},
-    "R(Obj*Obj,Nom)": {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.NominalA: [CCD.NominalA]},
-    "R(Obj*Obj,Ord)": {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.NominalA: [CCD.OrdinalA]},
-    "R(Obj*Obj,Ratio)": {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.NominalA: [CCD.RatioA]},
-    "R(Obj*Obj,Reg)": {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.LayerA: [CCD.LineA]},
-    "R(Obj,Reg)": {CCD.CoreConceptQ: [CCD.ObjectQ], CCD.LayerA: [CCD.VectorA]},
-    "R(Obj,Reg*Top)": {CCD.CoreConceptQ: [CCD.ObjectQ], CCD.LayerA: [CCD.VectorA]},
-    "R(Obj,Reg*Bool)": {CCD.CoreConceptQ: [CCD.ObjectQ], CCD.NominalA: [CCD.BooleanA], CCD.LayerA: [CCD.VectorA]},
-    "R(Obj,Reg*Count)": {CCD.CoreConceptQ: [CCD.ObjectQ], CCD.NominalA: [CCD.CountA], CCD.LayerA: [CCD.VectorA]},
-    "R(Obj,Reg*Itv)": {CCD.CoreConceptQ: [CCD.ObjectQ], CCD.NominalA: [CCD.IntervalA], CCD.LayerA: [CCD.VectorA]},
-    "R(Obj,Reg*Nom)": {CCD.CoreConceptQ: [CCD.ObjectQ], CCD.NominalA: [CCD.NominalA], CCD.LayerA: [CCD.VectorA]},
-    "R(Obj,Reg*Ord)": {CCD.CoreConceptQ: [CCD.ObjectQ], CCD.NominalA: [CCD.OrdinalA], CCD.LayerA: [CCD.VectorA]},
-    "R(Obj,Reg*Ratio)": {CCD.CoreConceptQ: [CCD.ObjectQ], CCD.NominalA: [CCD.RatioA], CCD.LayerA: [CCD.VectorA]},
-    "R(_,Ord)": {CCD.NominalA: [CCD.OrdinalA]},
-    "R(_,Ratio)": {CCD.NominalA: [CCD.RatioA]},
-    "R(_,Reg)": {CCD.LayerA: [CCD.VectorA]},
-    "R(Reg,_)": {CCD.LayerA: [CCD.VectorA]},
-    "R(Reg,Count)": {CCD.NominalA: [CCD.CountA], CCD.LayerA: [CCD.VectorA]},
-    "R(Reg,Itv)": {CCD.NominalA: [CCD.CountA], CCD.LayerA: [CCD.VectorA]},
-    "R(Reg,Nom)": {CCD.NominalA: [CCD.CountA], CCD.LayerA: [CCD.VectorA]},
-    "R(Reg*Obj,Itv)": {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.NominalA: [CCD.CountA], CCD.LayerA: [CCD.VectorA]},
-    "R(Reg*Obj,Nom)": {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.NominalA: [CCD.NominalA], CCD.LayerA: [CCD.VectorA]},
-    "R(Reg*Obj,Ord)": {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.NominalA: [CCD.OrdinalA], CCD.LayerA: [CCD.VectorA]},
-    "R(Reg*Obj,Ratio)": {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.NominalA: [CCD.RatioA], CCD.LayerA: [CCD.VectorA]},
-    "R(Reg,Ord)": {CCD.NominalA: [CCD.OrdinalA], CCD.LayerA: [CCD.VectorA]},
-    "R(Reg,Ratio)": {CCD.NominalA: [CCD.RatioA], CCD.LayerA: [CCD.VectorA]},
-}
+conversion = [
+    ("Reg", {CCD.LayerA: [CCD.RegionA]}),
+    ("C(Loc)", {CCD.LayerA: [CCD.RegionA]}),
+    ("R(Loc,Bool)", {CCD.CoreConceptQ: [CCD.FieldQ], CCD.NominalA: [CCD.BooleanA]}),
+    ("R(Loc,Itv)", {CCD.CoreConceptQ: [CCD.FieldQ], CCD.NominalA: [CCD.IntervalA]}),
+    ("R(Loc,Nom)", {CCD.CoreConceptQ: [CCD.FieldQ], CCD.NominalA: [CCD.NominalA]}),
+    ("R(Loc,Ord)", {CCD.CoreConceptQ: [CCD.FieldQ], CCD.NominalA: [CCD.OrdinalA]}),
+    ("R(Loc,Ratio)", {CCD.CoreConceptQ: [CCD.FieldQ], CCD.NominalA: [CCD.RatioA]}),
+    ("R(Obj*Obj,Itv)", {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.NominalA: [CCD.IntervalA]}),
+    ("R(Obj*Obj,Nom)", {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.NominalA: [CCD.NominalA]}),
+    ("R(Obj*Obj,Ord)", {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.NominalA: [CCD.OrdinalA]}),
+    ("R(Obj*Obj,Ratio)", {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.NominalA: [CCD.RatioA]}),
+    ("R(Obj*Obj,Reg)", {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.LayerA: [CCD.LineA]}),
+    ("R(Obj,Reg)", {CCD.CoreConceptQ: [CCD.ObjectQ], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Obj,Reg*Top)", {CCD.CoreConceptQ: [CCD.ObjectQ], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Obj,Reg*Bool)", {CCD.CoreConceptQ: [CCD.ObjectQ], CCD.NominalA: [CCD.BooleanA], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Obj,Reg*Count)", {CCD.CoreConceptQ: [CCD.ObjectQ], CCD.NominalA: [CCD.CountA], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Obj,Reg*Itv)", {CCD.CoreConceptQ: [CCD.ObjectQ], CCD.NominalA: [CCD.IntervalA], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Obj,Reg*Nom)", {CCD.CoreConceptQ: [CCD.ObjectQ], CCD.NominalA: [CCD.NominalA], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Obj,Reg*Ord)", {CCD.CoreConceptQ: [CCD.ObjectQ], CCD.NominalA: [CCD.OrdinalA], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Obj,Reg*Ratio)", {CCD.CoreConceptQ: [CCD.ObjectQ], CCD.NominalA: [CCD.RatioA], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Reg,Count)", {CCD.NominalA: [CCD.CountA], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Reg,Itv)", {CCD.NominalA: [CCD.CountA], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Reg,Nom)", {CCD.NominalA: [CCD.CountA], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Reg*Obj,Itv)", {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.NominalA: [CCD.CountA], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Reg*Obj,Nom)", {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.NominalA: [CCD.NominalA], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Reg*Obj,Ord)", {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.NominalA: [CCD.OrdinalA], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Reg*Obj,Ratio)", {CCD.CoreConceptQ: [CCD.NetworkQ], CCD.NominalA: [CCD.RatioA], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Reg,Ord)", {CCD.NominalA: [CCD.OrdinalA], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Reg,Ratio)", {CCD.NominalA: [CCD.RatioA], CCD.LayerA: [CCD.VectorA]}),
+    ("R(Reg,_)", {CCD.LayerA: [CCD.VectorA]}),
+    ("R(Obj,_)", {CCD.CoreConceptQ: [CCD.ObjectQ]}),
+    ("R(_,Reg)", {CCD.LayerA: [CCD.VectorA]}),
+    ("R(_,Ord)", {CCD.NominalA: [CCD.OrdinalA]}),
+    ("R(_,Itv)", {CCD.NominalA: [CCD.IntervalA]}),
+    ("R(_,Count)", {CCD.NominalA: [CCD.CountA]}),
+    ("R(_,Ratio)", {CCD.NominalA: [CCD.RatioA]}),
+    ("R(_,Nom)", {CCD.NominalA: [CCD.NominalA]}),
+]
 
 
 def cct2ccd(uri: URIRef) -> Polytype:
     t2 = cct.parse_type_uri(uri)
     dims = {d.root: d for d in ccd.dimensions}
-    for k, v in conversion.items():
+    for k, v in conversion:
         t = cct.parse_type(k)
 
         # This is a temporary solution: R(x * z, y) is for now
